f998f296b77b75d5dfde4b01fe5ce7bdefe7ccad
abdullahshaaban <abdullah.shaaban91@gmail.com>
2017-05-02 10:09:39 -0700

    updated changes

--- login_scraper_example.py
+++ login_scraper_example.py
@@ -1,38 +1,91 @@
 #from urllib.request import urlopen as uReq
 #import urllib.parse
-#from bs4 import BeautifulSoup as soup
+from bs4 import BeautifulSoup
 
 import requests
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 #x = urllib.request.urlopen('https://pythonprogramming.net')
 
 
-#print(x.read())
+
 try:
 
-    #Actually login
-    with requests.Session() as c:
-        url = 'https://www.activistshorts.com/users/sign_in'
-        USERNAME = 'bbliss@sandiego.edu'
-        PASSWORD = 'fsu-stu712bb'
-        login_page = c.get(url,verify=False)
-        website_token = c.cookies['_stealthystealth_session']
-        print(website_token)
-        headers = login_page.headers
-        print(headers)
+    #Stop remember me from showing up
+    chrome_options = Options()
+    chrome_options.add_experimental_option('prefs', {
+        'credentials_enable_service': False,
+        'profile': {
+            'password_manager_enabled': False
+        }
+    })
 
-        print(login_page.content)
-        print(login_page.status_code)
 
-        #Continue trying to fix this
-        login_data = {'authenticity_token': website_token, 'user[email]': USERNAME, 'user[password]': PASSWORD,
-                      'user[remember_me]': 0, 'commit': 'Sign In', 'utf8' : 1}
-        first_page = c.post(url, data=login_data, headers = headers)
-        print(first_page.content)
-        print(first_page.status_code)
-        #After loginURL
-        #next_page = c.post('https://www.activistshorts.com/dashboard')
+    #Open the chrome driver, and navigate to the page
+    driver = webdriver.Chrome("chromedriver/chromedriver.exe",chrome_options=chrome_options)
+    driver.get("https://www.activistshorts.com/users/sign_in")
+
+    #Isolate the input fields, and insert data to form
+    USERNAME = driver.find_element_by_css_selector('#user_email')
+    USERNAME.send_keys('bbliss@sandiego.edu')
+    PASSWORD = driver.find_element_by_css_selector('#user_password')
+    PASSWORD.send_keys('fsu-stu712bb')
+    LOGIN = driver.find_element_by_xpath("//input[@value='Sign in']")
+
+    #Submit Data
+    LOGIN.click()
+
+    #navigate to search result - remember to implement loop here
+    driver.get("https://www.activistshorts.com/search/results?company%5Btickers%5D=VRX")
+    time.sleep(3)
+
+    #Get to the last required page
+    link_to_search_result = driver.find_element_by_css_selector(".tablesorter td:nth-child(1)")
+    link_to_search_result.click()
+    time.sleep(3)
+
+    #Synopsis copy
+    time.sleep(3)
+    synopsis = driver.find_element_by_css_selector(".synopsis p")
+    print(synopsis.text)
+
+    #Bottom links counter
+    documents_table = driver.find_elements_by_xpath("//*[@id='203']/div/div[4]/div/div/div[8]/div/table/tbody/tr")
+
+    size_documents_table = len(documents_table)
+    time.sleep(3)
+    print(size_documents_table)
+
+    # #Actually login
+    # with requests.Session() as c:
+    #     #Initial login page info setup
+    #     url = 'https://www.activistshorts.com/users/sign_in'
+    #     USERNAME = 'bbliss@sandiego.edu'
+    #     PASSWORD = 'fsu-stu712bb'
+    #
+    #     #Login page call - HTML cleanup and hidden token extraction
+    #     login_page = c.get(url,verify=False)
+    #     login_page_html = BeautifulSoup(login_page.text, "lxml")
+    #     website_token= login_page_html.find_all('input', {'name': 'authenticity_token'})[0]['value']
+    #
+    #     login_data = {'authenticity_token': website_token, 'user[email]': USERNAME, 'user[password]': PASSWORD, 'user[remember_me]': 0,'user[remember_me]': 0, 'commit': 'Sign In', 'utf8' : '&#x2713'}
+    #     #login_data = dict(utf8='&#x2713', authenticity_token=website_token, user[email]=USERNAME, user[password]=PASSWORD,user[remember_me]=0,user[remember_me]=1,commit='Sign in')
+    #     first_page = c.post(url, data=login_data, headers={"Referer": "http://www.google.com"})
+    #
+    #     # Dashboard URL call
+    #     #dashboard_url = 'https://www.activistshorts.com/search/results?company%5Btickers%5D=VRX'
+    #
+    #     user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36'
+    #     headers = {'User-Agent': user_agent}
+    #
+    #
+    #     search_url = 'https://www.activistshorts.com/companies/203/campaigns/1018'
+    #     search_url = c.get(search_url, verify=False, headers=headers)
+    #     search_url_soup = BeautifulSoup(search_url.text, 'html.parser')
+    #     print(search_url_soup.prettify())
 
 except Exception as e:
     print(str(e))
